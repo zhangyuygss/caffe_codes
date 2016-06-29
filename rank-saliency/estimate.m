6959778aae8dfd4c2e2980dbfbc3fd66d8db5502
zhangyu <zhangyuygss@outlook.com>
2016-06-29 22:51:18 +0800

    s

--- rank-saliency/estimate.m
+++ rank-saliency/estimate.m
@@ -8,28 +8,37 @@ unsupervisedtxt = 'unsupervised.txt';
 
 sallist = dir([saldir_1 '*.jpg']);
 fid = fopen(unsupervisedtxt,'w+');
+gtresults = zeros(1,length(sallist));
+score = zeros(1,length(sallist));
 for i = 1:length(sallist)
 	sm = imread([saldir_1 sallist(i).name]);
 	img = imread([imgdir sallist(i).name]);
 	GT = imread([gtdir sallist(i).name(1:end-4) '.png']);
 	para = para_get(img,sm);
-	gtresults = QXL_ROC(sm,GT,100);
+	gtresults(i) = QXL_ROC(sm,GT,100);
+	score(i) = para(6);
 	fprintf(fid, '%s: ', sallist(i).name(1:end-4));
 	fprintf(fid, '%8.4f', para);
-	fprintf(fid, '%8.4f', gtresults);
+	fprintf(fid, '%8.4f', gtresults(i));
 	fprintf(fid, '\r\n');
 end
 
+
 sallist = dir([saldir_2 '*.jpg']);
+gtresults1 = zeros(1,length(sallist));
+score1 = zeros(1,length(sallist));
 fid = fopen(supervisedtxt,'w+');
 for i = 1:length(sallist)
 	sm = imread([saldir_2 sallist(i).name]);
 	img = imread([imgdir sallist(i).name]);
 	GT = imread([gtdir sallist(i).name(1:end-4) '.png']);
 	para = para_get(img,sm);
-	gtresults = QXL_ROC(sm,GT,100);
+	gtresults1(i) = QXL_ROC(sm,GT,100);
+	score1(i) = para(6);
 	fprintf(fid, '%s: ', sallist(i).name(1:end-4));
 	fprintf(fid, '%8.4f', para);
-	fprintf(fid, '%8.4f', gtresults);
+	fprintf(fid, '%8.4f', gtresults(i));
 	fprintf(fid, '\r\n');
 end
+
+xlswrite('compare.xlsx',gtresults',score',gtresults1',score1');
