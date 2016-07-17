f0a40a4fc147d56998d3539de9103fb1013dbe89
zhangyu <zhangyuygss@outlook.com>
2016-07-18 00:56:59 +0800

    717

--- vgg/evaluate_unsupervised.m
+++ vgg/evaluate_unsupervised.m
@@ -1,21 +1,21 @@
 clear;
-save_path = '/home/zhangyu/data/vggnet/unsupervised1_performance';
+save_path = '/home/zhangyu/data/hed-sal/dhs/performance';
 datadir = '/home/zhangyu/data/database/';
 datasets={'DUT-O','bsd','ECSSD'};%'MSRA10K'
-salMapRootDir='/home/zhangyu/data/vggnet/unsupervised/';
-allModel={'vgg_iter_5000','vgg_iter_10000','vgg_iter_15000',...
-        'vgg_iter_20000','vgg_iter_25000','vgg_iter_30000',...
-        'vgg_iter_35000','vgg_iter_40000','vgg_iter_45000',...
-        'vgg_iter_50000','vgg_iter_55000','vgg_iter_60000',...
-        'vgg_iter_65000','vgg_iter_70000','vgg_iter_75000',...
-        'vgg_iter_80000'};
+salMapRootDir='/home/zhangyu/data/hed-sal/dhs/';
+allModel={'usuperdhs_iter_5000','usuperdhs_iter_10000','usuperdhs_iter_15000',...
+          'usuperdhs_iter_20000','usuperdhs_iter_25000','usuperdhs_iter_30000',...
+          'usuperdhs_iter_35000','usuperdhs_iter_40000','usuperdhs_iter_45000',...
+          'usuperdhs_iter_50000','usuperdhs_iter_55000','usuperdhs_iter_60000',...
+          'usuperdhs_iter_65000','usuperdhs_iter_70000','usuperdhs_iter_75000',...
+          'usuperdhs_iter_80000','usuperdhs_iter_85000','usuperdhs_iter_90000','usuperdhs_iter_95000'};
 ext='.jpg';
 perfWrite={'AUC','AP','Pre1','TPR1','Fm'};
 for datasetIdx=1:length(datasets)
     results=[];
     datasetName=datasets{datasetIdx};
     disp(datasetName);
-    mkdir('/home/zhangyu/data/vggnet/unsupervised1_performance/',datasetName);
+    mkdir('/home/zhangyu/data/hed-sal/dhs/performance/',datasetName);
     maskPath=[datadir datasets{datasetIdx} '/GT/'];
     for modelIdx=1:length(allModel)
         modelName=allModel{modelIdx};
@@ -47,3 +47,59 @@ for datasetIdx=1:length(datasets)
     fclose(fid);
     
 end
+
+
+%plot performance
+clear;
+load('DUT-O_results.mat');
+auc = zeros(1,19);
+ap = auc;
+f = auc;
+pre = auc;
+tpr = auc;
+for i = 1:19
+    auc(i) = results(i).Performance.AUC;
+    ap(i) = results(i).Performance.AP;
+    pre(i) = results(i).Performance.Pre1;
+    tpr(i) = results(i).Performance.TPR1;
+    f(i) = results(i).Performance.Fm;
+end
+
+load('/home/zhangyu/data/vggnet/unsupervised_performance/DUT-O/DUT-O_results.mat');
+auc1 = zeros(1,9);
+ap1 = auc1;
+f1 = auc1;
+pre1 = auc1;
+tpr1 = auc1;
+for i = 1:9
+    auc1(i) = results(i).Performance.AUC;
+    ap1(i) = results(i).Performance.AP;
+    pre1(i) = results(i).Performance.Pre1;
+    tpr1(i) = results(i).Performance.TPR1;
+    f1(i) = results(i).Performance.Fm;
+end
+
+load('/home/zhangyu/data/vggnet/performance/DUT-O/DUT-O_results.mat');
+auc2 = zeros(1,19);
+ap2 = auc2;
+f2 = auc2;
+pre2 = auc2;
+tpr2 = auc2;
+for i = 1:9
+    auc2(i) = results(i).Performance.AUC;
+    ap2(i) = results(i).Performance.AP;
+    pre2(i) = results(i).Performance.Pre1;
+    tpr2(i) = results(i).Performance.TPR1;
+    f2(i) = results(i).Performance.Fm;
+end
+
+i = 5000:5000:95000;
+i1 = 5000:5000:45000;
+plot(i,auc,'r',i,ap,'b',i,pre,'g',i,tpr,'y',i,f,'c',...
+    i1,auc1,'r-',i1,ap1,'b-',i1,pre1,'g-',i1,tpr1,'y-',i1,f1,'c-',...
+    i,auc2,'r*',i,ap2,'b*',i,pre2,'g*',i,tpr2,'y*',i,f2,'c*');
+title('DUT-O Performance');
+xlabel('Iteration');
+ylabel('Performance');
+legend('AUC','AP','Pre','TPR','Fmeasure','AUC1','AP1','Pre1',...
+    'TPR1','Fmeasure1','AUC2','AP2','Pre2','TPR2','Fmeasure2');
--- vgg/get_smap.m
+++ vgg/get_smap.m
@@ -1,26 +1,27 @@
 %get saliency maps from trained models 
 addpath(genpath('/usr/local/caffe-master/matlab'))
-caffe.reset_all();
-caffe.set_mode_gpu();
-gpu_id = 1;
-caffe.set_device(gpu_id);
-model_dir = '/home/zhangyu/data/vggnet/snap/vgg/';
-def_dir = '/home/zhangyu/codes/caffe-codes/vgg/';
-phase = 'test'; % run with phase test (so that dropout isn't applied)
-net_model = [def_dir 'SO_global_vgg_deploy.prototxt'];
+
 datasets = {'ECSSD','bsd','DUT-O'};%,,'MSRA10K'
 
 %model loop should be outside the dataset loop, or GPU memory will be running out soon
-model_list = dir('/home/zhangyu/data/vggnet/snap/vgg/*.caffemodel');
+model_list = dir('/home/zhangyu/data/hed-sal/dhs/snap/*.caffemodel');
 for i = 1:length(model_list)
+    caffe.reset_all();
+    caffe.set_mode_gpu();
+    gpu_id = 1;
+    caffe.set_device(gpu_id);
+    model_dir = '/home/zhangyu/data/hed-sal/dhs/snap/';
+    def_dir = '/home/zhangyu/codes/caffe_codes/DHSnet/';
+    phase = 'test'; % run with phase test (so that dropout isn't applied)
+    net_model = [def_dir 'SO_RCL_deploy.prototxt'];
     modelname = model_list(i).name;
     net_weights = [model_dir modelname];
     net = caffe.Net(net_model, net_weights, phase);
-    for j = 1:length(datasets)
+    for j = 2:length(datasets)
         imgdir = strcat('/home/zhangyu/data/database/',datasets{j},'/images/');
-        tmpdir = ['/home/zhangyu/data/vggnet/unsupervised1/' datasets{j}];        %mkdir 2 arguments max
+        tmpdir = ['/home/zhangyu/data/hed-sal/dhs/' datasets{j}];        %mkdir 2 arguments max
         mkdir(tmpdir,model_list(i).name(1:end-11));
-        smdir = strcat('/home/zhangyu/data/vggnet/unsupervised1/',datasets{j},'/',model_list(i).name(1:end-11));
+        smdir = strcat('/home/zhangyu/data/hed-sal/dhs/',datasets{j},'/',model_list(i).name(1:end-11));
         disp(['Get saliency map: ' datasets{j} ' ' model_list(i).name]);
         imglist = dir([imgdir '*.jpg']);
         for idx = 1:length(imglist)
